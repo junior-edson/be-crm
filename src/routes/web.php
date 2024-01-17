608943147ed8211d81da0345da4b312b8630cdd3
Edson de Oliveira Junior <junior.edson@me.com>
2024-01-17 11:44:21 -0300

    Add clients to web route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Clients;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,4 +26,6 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/clients', Clients::class)->name('clients');
 });

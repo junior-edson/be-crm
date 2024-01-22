d9a20c3491a6752689693671d27fa9149a15a21c
Edson de Oliveira Junior <junior.edson@me.com>
2024-01-22 12:15:22 -0300

    Update create agenda event test

--- src/tests/Unit/Agenda/CreateAgendaEventTest.php
+++ src/tests/Unit/Agenda/CreateAgendaEventTest.php
@@ -10,13 +10,28 @@ use Tests\TestCase;
 
 class CreateAgendaEventTest extends TestCase
 {
-
     public function testCreateAgendaEventWithClient()
     {
         $client = Client::factory()->create();
 
         $payload = [
             'client_id' => $client->id,
+            'event_datetime' => Carbon::now()->addDays()->format('Y-m-d H:i'),
+            'address' => '123 Main St',
+            'description' => 'Test description',
+        ];
+        $request = new CreateAgendaEventRequest($payload);
+
+        $service = new CreateAgendaEventService();
+        $service->execute($request);
+
+        $this->assertDatabaseCount('agenda_events', 1);
+        $this->assertDatabaseHas('agenda_events', $payload);
+    }
+
+    public function testCreateAgendaEventWithoutClient()
+    {
+        $payload = [
             'event_datetime' => Carbon::now()->addDays()->format('Y-m-d H:i'),
             'address' => '123 Main St',
             'description' => 'Test description',
